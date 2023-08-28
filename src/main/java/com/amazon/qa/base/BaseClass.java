4656f8dcf870fe1cc8ad3ba4c8e0b10e0c521df4
poreysushant <sushantporey29@gmail.com>
2023-08-28 16:52:51 +0530

    last

--- src/main/java/com/amazon/qa/base/BaseClass.java
+++ src/main/java/com/amazon/qa/base/BaseClass.java
@@ -15,7 +15,7 @@ public class BaseClass
 	{         driver=new ChromeDriver();
 	          driver.manage().window().maximize();     
 	          driver.manage().window().maximize();    
-	          driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+  	          driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	          driver.get("https://www.amazon.in/");
 }
 }
